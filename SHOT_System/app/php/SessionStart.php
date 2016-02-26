cb15b9b3900c0bee7054c7754d0061e3d7282acf
Geo <geo.artemenko@gmail.com>
2016-02-26 18:17:49 -0500

    format
    This is just a code format update. It introduces no functional changes,
    but makes the code pretty, consistent, and PSR-compliant.
    
    1) always open with <?php
    
    2) use single quotes, unless otherwise necessary
    
    3) never close the file with ?>, just use a comment
    
    4) always try to leave an empty line at the end of files
    
    ...perhaps this description belongs in the Readme.md? Let me know if
    anyone finds it in here. ;-)

--- SHOT_System/app/php/SessionStart.php
+++ SHOT_System/app/php/SessionStart.php
@@ -1,10 +1,14 @@
 <?php
-  session_start();
-  require_once './../../keys.php';
-  $_SESSION["dbServer"] = $DB_Host;
-  $_SESSION["dbUser"] = $DB_User;
-  $_SESSION["dbPW"] = $DB_Pass;
-  $_SESSION["dbDatabase"] = $DB_Name;
 
-  header("Location: ../../app.html");
-?>
+session_start();
+
+require_once './../../keys.php';
+$_SESSION['dbServer'] = $DB_Host;
+$_SESSION['dbUser'] = $DB_User;
+$_SESSION['dbPW'] = $DB_Pass;
+$_SESSION['dbDatabase'] = $DB_Name;
+
+header('Location: ../../app.html');
+exit;
+
+/* End of file */

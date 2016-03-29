e30f8f834f914eebe9b6f9ce7fc90bb59f8f06af
Geo <geo.artemenko@gmail.com>
2016-03-28 20:55:00 -0400

    sanitize input

--- SHOT_System/app/php/getincidents.php
+++ SHOT_System/app/php/getincidents.php
@@ -16,76 +16,103 @@ $where = [];
 
 // search params
 $post = $_POST;
-// error_log(print_r($post, 1));
+// $post = $_REQUEST;
+// print_r($post);
+
+$bad = '#[^\w\d\s/-]#'; // legal only: word (A-Za-z_), digit (0-9), whitespace, forward slash, dash
 
 // partial incident name
-if (isset($post['name'])) {
+if ($success && isset($post['name'])) {
   $value = trim($post['name']); // just in case
-  if (strlen($value)) { // string
+  if (preg_match($bad, $post['name'])) {
+    $success = false;
+    trigger_error('Illegal input for name');
+  } elseif (strlen($value)) { // string
     $where[] = "i.Incident_Name LIKE '%$value%'"; // @TODO needs sanitation with mysqli->prepare
   }
 }
 
 // exact state region
-if (isset($post['region'])) {
+if ($success && isset($post['region'])) {
   $value = trim($post['region']);
-  if (strlen($value)) {
+  if (preg_match($bad, $post['region'])) {
+    $success = false;
+    trigger_error('Illegal input for region');
+  } elseif (strlen($value)) {
     $where[] = "st.Region = '$value'";
   }
 }
 
 // partial city
-if (isset($post['city'])) {
+if ($success && isset($post['city'])) {
   $value = trim($post['city']);
-  if (strlen($value)) {
+  if (preg_match($bad, $post['city'])) {
+    $success = false;
+    trigger_error('Illegal input for city');
+  } elseif (strlen($value)) {
     $where[] = "i.City LIKE '%$value%'";
   }
 }
 
 // exact state ID
-if (isset($post['state'])) {
-  $value = intval(trim($post['state']));
-  if ($value) { // must be a positive number
+if ($success && isset($post['state'])) {
+  $value = intval($post['state']);
+  if (preg_match($bad, $post['state'])) {
+    $success = false;
+    trigger_error('Illegal input for state');
+  } elseif ($value) { // must be a positive number
     $where[] = "i.State_ID = $value";
   }
 }
 
 // partial ZIP
-if (isset($post['zip'])) {
+if ($success && isset($post['zip'])) {
   $value = trim($post['zip']);
-  if (strlen($value)) {
+  if (preg_match($bad, $post['zip'])) {
+    $success = false;
+    trigger_error('Illegal input for zip');
+  } elseif (strlen($value)) {
     $where[] = "i.ZIP_CODE LIKE '%$value%'";
   }
 }
 
 // date_from
-if (isset($post['date_from'])) {
+if ($success && isset($post['date_from'])) {
   $value = trim($post['date_from']);
-  if (strlen($value)) {
+  if (preg_match($bad, $post['date_from'])) {
+    $success = false;
+    trigger_error('Illegal input for date_from');
+  } elseif (strlen($value)) {
     list($date) = explode('T', $value); // just in case
     $where[] = "i.Date_Occured >= '$date'";
   }
 }
 
 // date_to
-if (isset($post['date_to'])) {
+if ($success && isset($post['date_to'])) {
   $value = trim($post['date_to']);
-  if (strlen($value)) {
+  if (preg_match($bad, $post['date_to'])) {
+    $success = false;
+    trigger_error('Illegal input for date_to');
+  } elseif (strlen($value)) {
     list($date) = explode('T', $value);
     $where[] = "i.Date_Occured <= '$date'";
   }
 }
 
 // partial Subject (Suspect) Name
-if (isset($post['subject'])) {
+if ($success && isset($post['subject'])) {
   $value = trim($post['subject']);
-  if (strlen($value)) {
+  if (preg_match($bad, $post['subject'])) {
+    $success = false;
+    trigger_error('Illegal input for subject');
+  } elseif (strlen($value)) {
     $where[] = "su.Suspect_Name LIKE '%$value%'";
   }
 }
 
 // mysql request
-if ($where) {
+if ($success && $where) {
   $where = implode(' AND ', $where);
 
   $sql = "SELECT
@@ -109,10 +136,12 @@ if ($where) {
   if ( ! $mysqli_result) {
     // error_log($mysqli->error);
     trigger_error('Error Retrieving incidents from Database!');
+    $success = false;
   }
   if ($mysqli_result->num_rows > 300) {
     // error_log($num_rows);
     trigger_error('Too many incidents to return.  Please narrow your search!');
+    $success = false;
   }
 	while ($record = $mysqli_result->fetch_assoc()) {
 		$result[] = $record;
